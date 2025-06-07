f3f6d6faec0cbb7f351e1d28c15d45500aee405d
Dawle Datta Lingram <dattadawle71@gmail.com>
2025-06-07 22:00:18 +0530

    practice

---
+++ Country_Task/Params_Example/Program.cs
@@ -0,0 +1,11 @@
+﻿void printNumbers(params int[] numbers)
+{
+    foreach (int number in numbers)
+    {
+        Console.Write(number+ " ");
+    }
+    Console.WriteLine();
+}
+
+printNumbers(1,2,3,4,5);
+printNumbers(2,3,4,6,7);
